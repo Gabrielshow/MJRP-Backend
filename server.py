88ada254cf6065f962fff139922b6c01ff72798b
Gabriel <adefelashogbanmu@gmail.com>
2024-03-02 17:03:10 +0100

    adding some other functionalities

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-# possible python backend
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from mjrp_implementation import main
+import os
 
 app = Flask(__name__)
 
@@ -12,6 +12,17 @@ def calculate():
     # Call your main function with the parameters
     result = main(data['parameters'])
     return jsonify(result=result)
+
+# Serve favicon
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+# Serve index.html for root URL
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
 
 # for testing purposes only
 @app.route('/dummy', methods=['GET'])
@@ -26,7 +37,9 @@ def dummy_endpoint():
     }
     return jsonify(dummy_data)
 
+# Enable CORS for all routes
+CORS(app)
 
 if __name__ == '__main__':
-    CORS(app)
-    app.run(debug=True)
+    # Production Deployment
+    app.run(host='0.0.0.0', port=5000)  # Use appropriate port for production
